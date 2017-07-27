3bc8d18365fc3686ddc5494d585857ff0bb10d41
Summer <sst2bd@virginia.edu>
2017-07-27 15:03:26 +0000

    wasn't ppushing run.py

---
+++ run.py
@@ -0,0 +1,18 @@
+#!flask/bin/python
+#from flask import Flask
+from app import app
+import os
+
+#app = Flask(__name__)
+#if __name__ == '__main__':
+# app.debug = True
+# port = int(os.environ.get("PORT", 5000))
+# app.run(host='0.0.0.0', port=port)
+
+
+
+app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+
+
