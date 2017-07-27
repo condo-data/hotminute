606d02f9e078117c85fe5ba6f682676283ea6865
Summer <sst2bd@virginia.edu>
2017-07-27 15:31:09 +0000

    added static and output

--- run.py
+++ run.py
@@ -4,13 +4,12 @@ from app import app
 import os
 
 #app = Flask(__name__)
+#if __name__ == '__main__':
+# port = int(os.environ.get("PORT", 5000))
+# app.run(host='0.0.0.0', port=port, debug=True)
+
 if __name__ == '__main__':
- port = int(os.environ.get("PORT", 5000))
- app.run(host='0.0.0.0', port=port, debug=True)
-
-
-
-#app.run(host='0.0.0.0', port=8080, debug=True)
+ app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 
