9cd22286f9875f8c8dc0ba8d38358eef009ee375
Summer <sst2bd@virginia.edu>
2017-08-04 20:02:11 +0000

    mem leaks

--- app/condo_data.py
+++ app/condo_data.py
@@ -5,81 +5,68 @@ import re
 import mechanize
 import csv
 import os
-#import urllib
 from io import BytesIO
 import zipfile
-#import lxml
 from app import app
+from pympler.tracker import SummaryTracker
+tracker = SummaryTracker()
 
 def scrapeSinglePage(text, site):
     """Take all of the data from the html table and format it into 
     a list of lists to be easily processed later"""
     if site == "hud":
         strainer = SoupStrainer('table', {'width':"100%", 'border':"1", 'cellpadding':"2", 'cellspacing':"1"})
-        #"
     else:
         strainer = SoupStrainer('table', {"id":"searchForm:mainpanel", "cellpadding":"10", "cellspacing":"0", "class":"inputpanel"})
     soup = BeautifulSoup(text, parseOnlyThese=strainer)
-    
-    #soup = BeautifulSoup(text)
-    #print(soup)
-    #print(soup)
-
-    #table = 
-    #for tr in table:
-    #    print(tr)
-    
-
+    tracker.print_diff()
     str = ""
     count = 0 
     rows = soup.table.findAll('tr')
+    tracker.print_diff()
     for row in rows:
         cols = row.findAll('td')
         temp = ",".join([re.sub('\s{2,}', ' ',x.text).replace(",","") for x in cols]) + "\n"
+        del cols
         if site == "hud":
             if "CondoName" not in temp:
                 count+=1
                 str += temp
+                del temp
         else:
             #print(temp)
             if "," in temp and "Your search returned" not in temp and "Condo Name" not in temp:
                 count+=1
-                #print(temp)
-                #temp = temp.replace(",", "")
                 str += temp
-            #print(str)
-
+                del temp
+    del rows
+    del soup
+    del strainer
+    tracker.print_diff()
     return str , count
 
 
 def isThereNext(text):
     """Check there is a Next button on the page."""
     soup = BeautifulSoup(text)
-    #print(soup.text)
 
     if("[Next]" in soup.text):
-        #print("wat")
+        del soup
         return True
+    del soup
+    
     return False
 
 def getNext(text,br,num_condos):
     """Get the response from the next page of condo data"""
     br.select_form(nr=2)
-    #br.form.find_control("FSTATE").readonly = False
-    #br.form.find_control("maxRows").readonly = False
-    #br.form.find_control("FSTATE").value = "DC"
-    #br.form.find_control("maxRows").value = str(num_condos-50)
-    #form.set_all_readonly(False)
-    #br.form.controls.FSTATE = ["DC"]
-    #control.maxRows = [num_condos-50]
-    #for control in br.form.controls:
-    #    print control
     response = br.submit(type='image')
     text = response.read()
 
     return text
 
 def scraperNoScraping(state, site, reportType):
+    tracker.print_diff()
     if site == "va":
         url = "https://vip.vba.va.gov/portal/VBAH/VBAHome/condopudsearch?paf_portalId=default&paf_communityId=100002&paf_pageId=500002&paf_dm=full&paf_gear_id=800001&paf_gm=content&paf_ps=_rp_800001_condoName%3D1_%26_rp_800001_condoId%3D1_%26_ps_800001%3Dmaximized%26_pid%3D800001%26_rp_800001_county%3D1_%26_rp_800001_stateCode%3D1_" + state + "%26_pm_800001%3Dview%26_md_800001%3Dview%26_rp_800001_cpbaction%3D1_performSearchPud%26_st_800001%3Dmaximized%26_rp_800001_reportType%3D1_" + reportType + "%26_rp_800001_regionalOffice%3D1_%26_rp_800001_city%3D1_&_requestid=455594"
         if reportType == "details":
@@ -91,50 +78,47 @@ def scraperNoScraping(state, site, reportType):
         ans="CondoName,Condo ID /Submission,Address,County,ApprovalMethod,Compositionof Project,Comments,DocumentStatus,ManufacturedHousing,FHAConcentration,Status,StatusDate,ExpirationDate\n"
 
     print(state + " program starting")
-   # print(state)
-
+    
+    tracker.print_diff()
+    
     br = mechanize.Browser()
     br.open(url)
-
     response = br.response()
     
-
-    
+    tracker.print_diff()
     
     if site == "hud":
         br.select_form(name='condoform')
         br.form['fstate'] = [state,]
         response = br.submit()
-
+        
+    tracker.print_diff()
+    
     text = response.read()
     
-
-
+    tracker.print_diff()
+    
     regex = re.compile(r'[0-9]+ records')
     results = regex.findall(text)
-    #print results
-    #Your search returned 591 records
-
+ 
     num_condos = ""
     for x in results:
         num_condos = int(x.split(' ', 1)[0])
-    #print(num_condos)
-
     
     filename = state + "_Condo_Data.csv"
-    
+    tracker.print_diff()
     count = 0
     t0 = time.time()
     if "No records match all the selection criteria" not in text:
         tup = scrapeSinglePage(text,site)
-  
         ans += tup[0]  
         count += int(tup[1])
+        del tup
         msg = ""
 
         while isThereNext(text):
             try:
-                text = getNext(text,br,num_condos)  
+                text = getNext(text,br,num_condos) 
 
             except:
                 msg="Site error occured in reading data for " + state + ", not all data was retrieved."
@@ -144,8 +128,8 @@ def scraperNoScraping(state, site, reportType):
                 tup = scrapeSinglePage(text,site)
                 ans += tup[0]
                 count += int(tup[1])
-
-       
+                del tup
+                
 
         if site == "hud":
             reportType = ""
@@ -155,22 +139,24 @@ def scraperNoScraping(state, site, reportType):
             msg ="Site error occured in reading data for " + state + ", not all data was retrieved."
         elif count != num_condos*5 and reportType == "details":
             msg ="Site error occured in reading data for " + state + ", not all data was retrieved."
-            #print(count)
-            #print(num_condos)
-        #print(msg)
+        del num_condos
+        del count
     else:
         msg = "No records match the selection criteria for " + state + " no data was retrieved."
+    del text
+    tracker.print_diff()
     d = time.time() - t0
     if site == "va":
          ans = ans.encode('utf-8')
          ans = ans.replace("&nbsp", "")
-    #print(ans)
+    tracker.print_diff()
     print(state +" duration: %.2f s." % d)
-    #logger.info(state +" duration: %.2f s." % d)
+
     with open(app.static_folder+ "/output/" + filename, "wb") as file:
     #with open("static/output/" + filename, "wb") as file:
         file.write(ans)    
-    #print(msg)
+    tracker.print_diff()
+    del ans
     return msg
 
 #scraperNoScraping('AZ', "va", "summary")
--- app/views.py
+++ app/views.py
@@ -21,8 +21,6 @@ def index():
     form = CondoForm()
     va_form = VACondoForm()
     
-    
-    #futures = []
     futures[:] = []
     if form.submit1.data and form.validate_on_submit():
         state_selected = request.form['state']
@@ -36,19 +34,20 @@ def index():
             for state in states:
                 future = executor.submit(condo_data.scraperNoScraping, state[0], site, reportType)
                 futures.append(future)
+                
+            del states
 
         else:
             future = executor.submit(condo_data.scraperNoScraping, state_selected, site, reportType)
             futures.append(future)
 
-        
+            
         return redirect(url_for("load", state_selected=state_selected, site=site))    
         
     elif va_form.vasubmit.data and va_form.validate_on_submit():
         site = "va"
         state_selected = request.form.get('state')
         reportType = request.form.get('rt')
-        print(state_selected)
         
         if state_selected == "ALL":
             states = app.config["VA_STATES"]
@@ -57,6 +56,8 @@ def index():
             for state in states:
                 future = executor.submit(condo_data.scraperNoScraping, state[0], site, reportType)
                 futures.append(future)
+                
+            del states
 
         else:
             future = executor.submit(condo_data.scraperNoScraping, state_selected, site, reportType)
@@ -80,27 +81,29 @@ def downloadpage(state_selected=None, site=None):
     fileType = ""
     origin = site.upper()
     
-    
+
     if state_selected == 'ALL':
         fileType = ".zip"
     else:
         fileType = '.csv'
-    
+
     mydate = datetime.datetime.now()
     month = mydate.strftime("%B")
     day = mydate.strftime('%d')
     year = mydate.year
     fn = str(month) +  "_" +  str(day)+ "_" + str(year) + "_" + state_selected + "_" + origin + "_Condo_Data" + fileType
+ 
+    del origin
+    del mydate
+    del month
+    del year
 
-    #get all messages 
+  
     for x in futures:
-        #print(x.result)
-        #print(x.result)
         if len(x.result()) > 1:
-            
             msgs.append(x.result()) 
-            #print(msgs)
-    
+
+
     if form.validate_on_submit():
         return redirect(url_for("download", state_selected=state_selected, filename=fn)) 
 
@@ -108,6 +111,7 @@ def downloadpage(state_selected=None, site=None):
     
 @app.route('/download/<state_selected>/<filename>', methods=['GET', 'POST'])
 def download(state_selected=None, filename=None):
+    
     directory = app.static_folder + "/output/"
     if state_selected == 'ALL':
         memory_file = BytesIO()
@@ -121,35 +125,27 @@ def download(state_selected=None, filename=None):
         with zipfile.ZipFile(memory_file, 'wb', zipfile.ZIP_DEFLATED) as zf:
             for individualFile in files:
                 zf.write(directory + individualFile, "/"+individualFile)
-
-        #memory_file.seek(0)
+        del files
+        
+        memory_file.seek(0)
         return send_file(memory_file, attachment_filename=filename, as_attachment=True)
     else: 
         attachment_filename = filename
-        
+    
     fn = state_selected + "_Condo_Data.csv"
     return send_from_directory(directory=directory, filename=fn, as_attachment=True, attachment_filename=attachment_filename)
 
 @app.route('/done/', methods=['GET', "POST"])
 def isDone():
+    
     state_selected  = request.args.get('state_selected', None)
     site  = request.args.get('site', None)
     
-    #print(futures.ALL_C)
-    print("LENGTH "+str(len(futures)))
     for x in futures:
-        
         if x.done():
             futures.remove(x)
-    print("LENGTH "+str(len(futures)))
-        
-
-    isDone = set([x.done() for x in futures])
-    
-    #if len(isDone) == 1 and True in isDone:
+    ]
     if len(futures) < 1:
-        #print(futures)
         del futures[:]  
-        #print(futures)
-        tracker.print_diff()
+        
         return jsonify({'state_selected': state_selected, 'result':url_for("downloadpage", state_selected=state_selected, site=site)})
