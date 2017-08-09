327a1ecb66df7c480867e2cd8a1ca8ab570a7f3a
Summer <sst2bd@virginia.edu>
2017-08-09 19:36:35 +0000

    D

--- app/views.py
+++ app/views.py
@@ -156,6 +156,8 @@ def isDone():
         if x.done():
             futures.remove(x)
             if state_selected == "ALL" and len(states) > 0:
+                print(states[0][0])
+                print(reportType)
                 futures.append(executor.submit(condo_data.scraperNoScraping, states[0][0], site, reportType))
                 states.remove(states[0])
     print(futures)
