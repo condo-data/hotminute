2544ada6d2ae655058787c8d77b054c61023ef8e
Summer <sst2bd@virginia.edu>
2017-08-09 13:10:33 +0000

    flask compress

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_compress import Compress
 
-app = Flask(__name__, static_folder='static')
-app.config.from_object('config')
+
+#app.config.from_object('config')
+#Compress(app)
+
+compress = Compress()
+
+def start_app():
+    app = Flask(__name__, static_folder='static')
+    app.config.from_object('config')
+    compress.init_app(app)
+    return app
 
 import views
